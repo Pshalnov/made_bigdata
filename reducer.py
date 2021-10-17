dc551fcae5eb3d1a71d3632464eeed2ab58c93ac
Pshalnov <30325807+Pshalnov@users.noreply.github.com>
2021-10-17 22:24:38 +0300

    reducer

---
+++ reducer.py
@@ -0,0 +1,15 @@
+import sys
+
+result_size = 0
+result_mean = 0
+result_var = 0
+
+for i in sys.stdin:
+    i = i.strip()
+    buff, mean, var = i.split('\t')
+
+    result_var = (result_size * result_var + buff * var) / (result_size + buff) + result_size * buff * ((result_mean - mean) / (result_size + buff)) ** 2
+    result_mean = (result_size * result_mean + buff * mean) / (result_size + buff)
+    result_size += buff
+
+print('%f\t%f' % (result_mean, result_var))
